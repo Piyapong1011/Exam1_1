e1a1fdfc3d751de29b798a57b5ad1d70718c18d7
Setsune <Piyapong.natt@gmail.kmutt.sc.th>
2023-03-13 16:53:12 +0700

    Exam1_1 finished

--- Program.cs
+++ Program.cs
@@ -25,6 +25,7 @@ namespace Exam1{
             float p2h = float.Parse(Console.ReadLine());
             Console.WriteLine("------------------------------");
             
+            float high, wide ;
             float p1rex, p1lex, p1tey, p1bey;
             float p2rex, p2lex, p2tey, p2bey;
             p1rex = p1x + p1w;
@@ -37,11 +38,214 @@ namespace Exam1{
             p2tey = p2y + p2h;
             p2bey = p2y - p2h;
 
-
-            /*while (p1x < p2x && p1y < p2y){
-
-                break;
-            }*/
+            //north east
+            if (p1x < p2x && p1y < p2y){
+                if (p2lex < p1rex && p2bey < p1tey){
+                    wide = p1rex - p2lex;
+                    high = p1tey - p2bey;
+                    if (wide * high > 8){
+                        Console.WriteLine("Too Much Overlaping");
+                    }
+                    else {
+                        Console.WriteLine("Not Much Overlaping");
+                    }
+                }
+                else {
+                    Console.WriteLine("No Overlapping");
+                }
+            }
+            //east
+            else if (p1x < p2x && p1y == p2y){
+                if (p2lex < p1rex){
+                    wide = p1rex - p2lex;
+                    if (p1h > p2h){
+                        high = p2h * 2;
+                        if (wide * high > 8){
+                            Console.WriteLine("Too Much Overlaping");
+                        }
+                        else {
+                            Console.WriteLine("Not Much Overlaping");
+                        }
+                    }
+                    else if (p1h < p2h){
+                        high = p1h * 2;
+                        if (wide * high > 8){
+                            Console.WriteLine("Too Much Overlaping");
+                        }
+                        else {
+                            Console.WriteLine("Not Much Overlaping");
+                        }
+                    }
+                    else {
+                        high = p1h * 2;
+                        if (wide * high > 8){
+                            Console.WriteLine("Too Much Overlaping");
+                        }
+                        else {
+                            Console.WriteLine("Not Much Overlaping");
+                        }
+                    }
+                }
+                else {
+                    Console.WriteLine("No Overlapping");
+                }
+            }
+            //south east
+            else if (p1x < p2x && p1y > p2y){
+                if (p2lex < p1rex && p2tey > p1bey){
+                    wide = p1rex - p2lex;
+                    high = p2tey - p1bey;
+                    if (wide * high > 8){
+                        Console.WriteLine("Too Much Overlaping");
+                    }
+                    else {
+                        Console.WriteLine("Not Much Overlaping");
+                    }
+                }
+                else {
+                    Console.WriteLine("No Overlapping");
+                }
+            }
+            //south
+            else if (p1x == p2x && p1y > p2y){
+                if (p2tey > p1tey){
+                    high = p2tey - p1tey;
+                    if (p1w > p2w){
+                        wide = p2w * 2;
+                        if (wide * high > 8){
+                            Console.WriteLine("Too Much Overlaping");
+                        }
+                        else {
+                            Console.WriteLine("Not Much Overlaping");
+                        }
+                    }
+                    else if (p1w < p2w){
+                        wide = p1w * 2;
+                        if (wide * high > 8){
+                            Console.WriteLine("Too Much Overlaping");
+                        }
+                        else {
+                            Console.WriteLine("Not Much Overlaping");
+                        }
+                    }
+                    else {
+                        wide = p1w * 2;
+                        if (wide * high > 8){
+                            Console.WriteLine("Too Much Overlaping");
+                        }
+                        else {
+                            Console.WriteLine("Not Much Overlaping");
+                        }
+                    }
+                }
+                else {
+                    Console.WriteLine("No Overlapping");
+                }
+            }
+            //south west
+            else if (p1x > p2x && p1y > p2y){
+                if (p2rex > p1lex && p2tey > p1bey){
+                    wide = p2rex - p1lex;
+                    high = p2tey - p1bey;
+                    if (wide * high > 8){
+                        Console.WriteLine("Too Much Overlaping");
+                    }
+                    else {
+                        Console.WriteLine("Not Much Overlaping");
+                    }
+                }
+                else {
+                    Console.WriteLine("No Overlapping");
+                }
+            }    
+            //west
+            else if (p1x > p2x && p1y == p2y){
+                if (p2rex > p1lex){
+                    wide = p2rex - p1lex;
+                    if (p1h > p2h){
+                        high = p2h * 2;
+                        if (wide * high > 8){
+                            Console.WriteLine("Too Much Overlaping");
+                        }
+                        else {
+                            Console.WriteLine("Not Much Overlaping");
+                        }
+                    }
+                    else if (p1h < p2h){
+                        high = p1h * 2;
+                        if (wide * high > 8){
+                            Console.WriteLine("Too Much Overlaping");
+                        }
+                        else {
+                            Console.WriteLine("Not Much Overlaping");
+                        }
+                    }
+                    else {
+                        high = p1h * 2;
+                        if (wide * high > 8){
+                            Console.WriteLine("Too Much Overlaping");
+                        }
+                        else {
+                            Console.WriteLine("Not Much Overlaping");
+                        }
+                    }
+                }
+                else {
+                    Console.WriteLine("No Overlapping");
+                }
+            }
+            //north west
+            else if (p1x > p2x && p1y < p2y){
+                if (p2rex > p1lex && p2bey < p1tey){
+                    wide = p2rex - p1lex;
+                    high = p1tey - p2bey;
+                    if (wide * high > 8){
+                        Console.WriteLine("Too Much Overlaping");
+                    }
+                    else {
+                        Console.WriteLine("Not Much Overlaping");
+                    }
+                }
+                else {
+                    Console.WriteLine("No Overlapping");
+                }
+            }
+            //north
+            else if (p1x == p2x && p1y < p2y){
+                if (p2bey < p1tey){
+                    high = p1tey - p2bey;
+                    if (p1w > p2w){
+                        wide = p2w * 2;
+                        if (wide * high > 8){
+                            Console.WriteLine("Too Much Overlaping");
+                        }
+                        else {
+                            Console.WriteLine("Not Much Overlaping");
+                        }
+                    }
+                    else if (p1w < p2w){
+                        wide = p1w * 2;
+                        if (wide * high > 8){
+                            Console.WriteLine("Too Much Overlaping");
+                        }
+                        else {
+                            Console.WriteLine("Not Much Overlaping");
+                        }
+                    }
+                    else {
+                        wide = p1w * 2;
+                        if (wide * high > 8){
+                            Console.WriteLine("Too Much Overlaping");
+                        }
+                        else {
+                            Console.WriteLine("Not Much Overlaping");
+                        }
+                    }
+                }
+                else {
+                    Console.WriteLine("No Overlapping");
+                }
+            }
         }
     }
 }
